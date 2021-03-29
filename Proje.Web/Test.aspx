9b0e85704ce74bc9dc4d55980ec071c3c4aa6992
sevval-code <erkilincsevval@gmail.com>
2021-03-29 17:46:33 +0300

    katmanlı mimari tasarımı

---
+++ Proje.Web/Test.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="Proje.Web.Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            DropDownList<br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <asp:DropDownList ID="drp_list" runat="server"></asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Proje.Web/Test.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proje.Web
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /* DropDown Ornegi
+             ListItem deger = new ListItem();
+            deger.Text = TextBox1.Text;
+            deger.Value = new Random().Next(0,50).ToString();
+            drp_list.Items.Add(deger);
+            */
+        }
+    }
+}
